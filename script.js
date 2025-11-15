e271be2769cebce4ee2c47b5d2e1c196667fec11
Farh Kadry <23-101289@students.eui.edu.eg>
2025-11-15 13:46:42 +0200

    hero and bgm trial

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let audio = document.getElementById("BGM");
 
+function playAudio() {
+  audio.play();
+};
+
+function pauseAudio() {
+  audio.pause();
+};
+const btn = document.getElementById("myBtn");
+
+function toggleOpacity(next) {
+  element.classList.toggle("fadedClick");
+};

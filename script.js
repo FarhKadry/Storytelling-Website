92baa6b3f23ad7f5df65c971250f902f654ce10f
Farh Kadry <23-101289@students.eui.edu.eg>
2025-11-18 01:14:23 +0200

    js: added character array

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-
+let characters = [
+    {
+        image: `public/images/character01.jpg`,
+        name: `Natasha Romanoff`,
+        job: `Agent, avenger, Assasin`,
+        tag: `Protagonist`
+    },
+    {
+        image: `public/images/character03.jpg`,
+        name: `Isaiah Ross`,
+        job: `Black Widow's counselor`,
+        tag: `Supporting`
+    },
+    {
+        image: `public/images/character02.jpg`,
+        name: `Mr. lucas`,
+        job: `Businessman`,
+        tag: `Antagonist`
+    },
+];
+for (let i=0; i < characters.length; i++){
+    document.getElementById("character_array").innerHTML+=`
+    <div class="character_card flex_column reveal_text">
+                <img class="img_stroke" src="${characters[i].image}" alt="">
+                <h3>${characters[i].name}</h3>
+                <h5>${characters[i].job}</h5>
+                <div class="tag"><h5>${characters[i].tag}</h5></div>
+            </div>
+    `
+}
